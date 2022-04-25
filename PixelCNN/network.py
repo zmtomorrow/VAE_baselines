fb0e6ef9c2207d510b57e35a72178dfccd836b09
Zonghao Chen <47094594+hudsonchen@users.noreply.github.com>
2022-04-25 23:01:30 +0800

    add_pixel_cnn

---
+++ PixelCNN/network.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from tqdm import tqdm
+from .module import GatedMaskedConv, VerticalStackConvolution, HorizontalStackConvolution
+
+
+class PixelCNN(nn.Module):
+
+    def __init__(self, c_in, c_hidden, layer_num):
+        super().__init__()
+        self.c_in = c_in
+        self.c_hidden = c_hidden
+        # Initial convolutions skipping the center pixel
+        self.conv_vstack = VerticalStackConvolution(c_in, c_hidden, mask_center=True)
+        self.conv_hstack = HorizontalStackConvolution(c_in, c_hidden, mask_center=True)
+        # Convolution block of PixelCNN. We use dilation instead of downscaling
+        self.conv_layers = nn.ModuleList()
+        for i in range(layer_num):
+            self.conv_layers.append(GatedMaskedConv(c_hidden))
+        # Output classification convolution (1x1)
+        self.conv_out = nn.Conv2d(c_hidden, c_in, kernel_size=(1, 1), padding=0)
+
+    def forward(self, x):
+        """
+        Forward image through model and return logits for each pixel.
+        Inputs:
+            x - Image tensor with integer values between 0 and 255.
+        """
+        # Scale input from 0 to 255 back to -1 to 1
+        # x = (x.float() / 255.0) * 2 - 1
+
+        # Initial convolutions
+        v_stack = self.conv_vstack(x)
+        h_stack = self.conv_hstack(x)
+        # Gated Convolutions
+        for layer in self.conv_layers:
+            v_stack, h_stack = layer(v_stack, h_stack)
+        # 1x1 classification convolution
+        # Apply ELU before 1x1 convolution for non-linearity on residual connection
+        out = self.conv_out(F.elu(h_stack))
+
+        # Output dimensions: [Batch, Channels, Height, Width]
+        return out[:, :int(self.c_in / 2), :, :], F.softplus(out[:, int(self.c_in / 2):, :, :])
+
+    @torch.no_grad()
+    def sample(self, img_shape, device, img=None):
+        """
+        Sampling function for the autoregressive model.
+        Inputs:
+            img_shape - Shape of the image to generate (B,C,H,W)
+            img (optional) - If given, this tensor will be used as
+                             a starting image. The pixels to fill
+                             should be -1 in the input tensor.
+        """
+        # Create empty image
+        if img is None:
+            img = 0.1 * torch.ones(img_shape, dtype=torch.float).to(device)
+        # Generation loop
+        for h in range(img_shape[2]):
+            for w in range(img_shape[3]):
+                pred_mean, pred_std = self.forward(img[:, :, :h + 1, :])
+                pred = torch.cat((pred_mean, pred_std), dim=1)
+                img[:, :, h, w] = pred[:, :, h, w]
+        gen_z_mean = img[:, :int(self.c_in / 2), :, :]
+        gen_z_std = img[:, int(self.c_in / 2):, :, :]
+        epsilon = torch.randn_like(gen_z_mean)
+        gen_z = epsilon * gen_z_std + gen_z_mean
+        return gen_z
--- network.py
+++ network.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
 class Residual(nn.Module):
     def __init__(self, channels):
         super(Residual, self).__init__()
@@ -23,7 +22,7 @@ class Residual(nn.Module):
 class fc_encoder(nn.Module):
     def __init__(self, channels=256, latent_channels=64):
         super(fc_encoder, self).__init__()
-        self.latent_channels=latent_channels
+        self.latent_channels = latent_channels
         self.encoder = nn.Sequential(
             nn.Conv2d(3, channels, 4, 2, 1, bias=False),
             nn.BatchNorm2d(channels),
@@ -32,19 +31,21 @@ class fc_encoder(nn.Module):
             nn.BatchNorm2d(channels),
             Residual(channels),
             Residual(channels),
-            nn.Conv2d(channels, latent_channels*2, 1)
+            nn.Conv2d(channels, latent_channels * 2, 1)
         )
 
     def forward(self, x):
-        z=self.encoder(x)
-        return z[:,:self.latent_channels,:,:].view(x.size(0),-1),F.softplus(z[:,self.latent_channels:,:,:].view(x.size(0),-1))
+        z = self.encoder(x)
+        # print(z.shape)
+        return z[:, :self.latent_channels, :, :].view(x.size(0), -1), F.softplus(
+            z[:, self.latent_channels:, :, :].view(x.size(0), -1))
 
 
 class fc_decoder(nn.Module):
     def __init__(self, channels=256, latent_channels=64, out_channels=100):
         super(fc_decoder, self).__init__()
         self.decoder = nn.Sequential(
-            nn.Conv2d( latent_channels, channels, 1, bias=False),
+            nn.Conv2d(latent_channels, channels, 1, bias=False),
             nn.BatchNorm2d(channels),
             Residual(channels),
             Residual(channels),
@@ -58,5 +59,5 @@ class fc_decoder(nn.Module):
         )
 
     def forward(self, z):
-#         print('here',z.size(0))
-        return  self.decoder(z.view(z.size(0),-1,8,8))
+        # print('here', z.shape)
+        return self.decoder(z.view(z.size(0), -1, 8, 8))
