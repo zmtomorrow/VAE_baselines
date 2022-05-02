10d4b929304aa16274b749fff10ed1a327c20904
Mingtian Zhang <zmt534885880@gmail.com>
2022-05-02 23:00:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -54,6 +54,34 @@ class VAE(nn.Module):
             eps = torch.randn(num,self.z_dim).to(self.device)
             pxz_params = self.decoder(eps)
             return self.sample_op(pxz_params)
+        
+        
+        
+class GaussVAE(nn.Module):
+    def __init__(self,opt):
+        super().__init__()
+        self.device=opt['device']
+        self.encoder=densenet_encoder(input_dim=opt['z_dim'], z_dim=50)
+        self.decoder=densenet_decoder(o_dim=opt['z_dim']*2, z_dim=50)
+        self.prior_mu=torch.zeros(50, requires_grad=False)
+        self.prior_std=torch.ones(50, requires_grad=False)
+        self.params = list(self.parameters())
+        
+    def forward(self, z_mu,z_std,spread_std=None):
+        eps=1e-7
+        if spread_std !=None:
+            z_std=torch.sqrt(z_std**2+torch.ones_like(z_std)*spread_std**2)
+        z_samples=torch.randn_like(z_mu)*z_std+z_mu
+        s_mu, s_std = self.encoder(z_samples.to(self.device))
+        s_samples =torch.randn_like(s_mu)*s_std+s_mu
+        z_hat_mu,z_hat_std = self.decoder(s_samples.to(self.device))
+        if spread_std !=None:
+            z_hat_std=torch.sqrt(z_hat_std**2+torch.ones_like(z_hat_std)*spread_std**2)
+        loglikelihood=-(torch.log(z_hat_std + eps) + (z_std ** 2 + (z_mu - z_hat_mu) ** 2) / (2 * z_hat_std ** 2)).sum(1).mean(0)
+
+        kl = batch_KL_diag_gaussian_std(s_mu,s_std,self.prior_mu.to(self.device),self.prior_std.to(self.device))
+        elbo = loglikelihood - kl
+        return torch.mean(elbo)
 
 
 
